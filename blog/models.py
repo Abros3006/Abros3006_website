a11c2aee029ff6b5b209891e9249ed63de849ded
Atharva Anil Bangle <152085024+Abros3006@users.noreply.github.com>
2024-07-25 22:31:42 +0530

    Snapshots

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    Date = models.DateTimeField(default=datetime.now, blank=True)
+    body = models.CharField(max_length=100000)
