78e9312b630cb3d6dc5b62fe0b44303322b0b625
Atharva Anil Bangle <152085024+Abros3006@users.noreply.github.com>
2024-07-26 20:19:51 +0530

    backend

--- snapshots/models.py
+++ snapshots/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Story(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField(default=datetime.now, blank=True)
+    Caption = models.CharField(max_length=100)
