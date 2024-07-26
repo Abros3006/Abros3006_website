ded3b2877fb08b22903152fc3d30621e512ca4ea
Atharva Anil Bangle <152085024+Abros3006@users.noreply.github.com>
2024-07-26 21:48:35 +0530

    Snapshots backend

--- snapshots/views.py
+++ snapshots/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Story
 # Create your views here.
 
 def snapshots(request):
-    return render(request, 'snapshots.html')
+    storys = Story.objects.all()
+    return render(request, 'snapshots.html', {'storys':storys})
