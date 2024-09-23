a254446892f7a2ffe77e87dc856dde5d60c74348
Atharva Bangle <atharvabangle@gmail.com>
2024-09-23 11:33:36 +0530

    edited blog post

--- blog/views.py
+++ blog/views.py
@@ -8,13 +8,6 @@ def blog(request):
     posts = Post.objects.all()
     return render(request, 'blog.html', {'posts': posts})
 
-def index(request):
-    posts = Post.objects.order_by('-date')[:3]
-    print(f"Number of posts retrieved: {len(posts)}")
-    for post in posts:
-        print(f"Post ID: {post.id}, Title: {post.title}, Date: {post.date}")
-    return render(request, 'index.html', {'posts': posts})
-
 def post(request, post_id):
     posts = Post.objects.get(id=post_id)
     return render(request, 'post.html', {'posts': posts})
