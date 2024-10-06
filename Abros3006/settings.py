513f884ff6afba94735237c16ed3a812c681dcc2
Atharva Bangle <atharvabangle@gmail.com>
2024-10-06 10:55:18 +0530

    added host and connection to database

--- Abros3006/settings.py
+++ Abros3006/settings.py
@@ -112,12 +112,17 @@ WSGI_APPLICATION = 'Abros3006.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('PGDATABASE'),
+        'USER': config('PGUSER'),
+        'PASSWORD': config('PGPASSWORD'),
+        'HOST': config('PGHOST'),
+        'PORT': config('PGPORT'),
+        'OPTIONS': {
+            'sslmode': 'require',  # Ensure SSL is enabled
+        },
     }
 }
-
-DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
 
 
 # Password validation
