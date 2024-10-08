767ec77ad731e3fde28a28d16740349ae09b91c8
Atharva Bangle <atharvabangle@gmail.com>
2024-10-08 22:20:21 +0530

    removed database url

--- Abros3006/settings.py
+++ Abros3006/settings.py
@@ -118,9 +118,6 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
