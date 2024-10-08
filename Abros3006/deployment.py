bf4765b90a39af20af36b4594fe67739d5a4787b
Atharva Bangle <atharvabangle@gmail.com>
2024-10-08 23:43:57 +0530

    done maybe

--- Abros3006/deployment.py
+++ Abros3006/deployment.py
@@ -3,10 +3,10 @@ from .settings import *
 from .settings import BASE_DIR
 from decouple import config
 
-SECRET_KEY = [config('SECRET_KEY')]
+SECRET_KEY = config('SECRET_KEY')  # Changed to remove the list
 ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', '')]
 
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ.get('WEBSITE_HOSTNAME', '')]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('WEBSITE_HOSTNAME', '')]
 DEBUG = False
 
 MIDDLEWARE = [
@@ -28,20 +28,35 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 connection_string = os.environ.get('AZURE_DATABASE_URL')
 parameters = {}
+
 if connection_string:
-    for pair in connection_string.split(' '):
+    pairs = connection_string.split(' ')
+    for pair in pairs:
         if '=' in pair:
             key, value = pair.split('=', 1)  # Use maxsplit=1 to handle '=' in values
-            parameters[key] = value
+            parameters[key.strip()] = value.strip()  # Stripping whitespace
         else:
             print(f"Warning: '{pair}' is not a valid key=value pair.")
+
+# Debugging output for parsed parameters
+print("Parsed database parameters:", parameters)
+
+# Validate required keys
+required_keys = ['dbname', 'host', 'port', 'user', 'password']
+for key in required_keys:
+    if key not in parameters:
+        raise ValueError(f"Missing required database parameter: {key}")
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': parameters['dbname'],
         'HOST': parameters['host'],
+        'PORT': parameters.get('port', '5432'),  # Default to 5432 if not provided
         'USER': parameters['user'],
         'PASSWORD': parameters['password'],
+        'OPTIONS': {
+            'sslmode': 'require',  # Set SSL mode as required
+        },
     }
 }
