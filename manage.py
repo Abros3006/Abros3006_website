e3acfc9f1ff477b03683ed70ec16620ce5b0794d
Atharva Bangle <atharvabangle@gmail.com>
2024-10-11 23:05:06 +0530

    k

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Abros3006.settings')
+    settings_module = "Abros3006.deployment"
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
